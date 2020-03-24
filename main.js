179ad015facc89b8118d39d62d887b4979693a16
EngrZaks <abzakariyya@gmail.com.com>
2020-03-24 20:12:22 +0100

    these changes kickstarted the addition of night mode and i also decided to use css class for my colors

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 
-		// function ShowSignUp() {
-			
-		// 	document.getElementById('signup_content').style.height = "800px";
-		// 	document.getElementById('signup_content').style.padding = "12px";
-		// }
-		// function closeSignUp() {
-		// 	document.getElementById('signup_content').style.height = "0";
-		// 	document.getElementById('signup_content').style.padding = "0";
-        // }
         
 	function ShowSignUp() {
         var x = document.getElementById("signup_content");
@@ -17,5 +8,27 @@
         } else {
             x.style.display = "none"; 
             x.style.padding = "0";
+        }
+    }
+
+    // FOR THE THEMES
+
+    var isdark;
+    function changeTheme() {
+        var logo = document.getElementById("logo");
+        var header = document.getElementById("header");
+        var main = document.getElementById("main");
+        var body = document.getElementById("body");
+        if (!isdark) {
+            header.setAttribute("class", "darktheme");
+            main.setAttribute("class", "darktheme");
+            body.setAttribute("class", "darktheme");
+            logo.style.color = "black"; 
+            isdark = true;
+        } else{
+            header.removeAttribute("class", "darktheme");
+            main.removeAttribute("class", "darktheme");
+            body.removeAttribute("class", "darktheme");
+            isdark = false;
         }
     }
